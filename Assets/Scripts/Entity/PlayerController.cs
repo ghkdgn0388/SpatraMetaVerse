a9eb2609518a51685648f8eb4271b4557de80205
ghkdgn0388 <mbc0388@naver.com>
2025-07-25 01:08:45 +0900

    [Feat]Hit&Damaged_추가

--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -34,4 +34,10 @@ public class PlayerController : BaseController
             lookDirection = lookDirection.normalized;
         }
     }
+
+    public override void Death()
+    {
+        base.Death();
+        gameManager.GameOver();
+    }
 }
